195ea9e480ceab14aec16397eaab8bf6ab1d743b
Prateet Tiwari <prateettiwari29@gmail.com>
2026-09-07 15:50:03 +0530

    Implement socket creation in main function

--- src/main.c
+++ src/main.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
 
 int main(void)
 {
-  printf("Celestial started...");
-  return 0;
+  int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+  if (server_fd == -1)
+  {
+    perror("socket");
+    return EXIT_FAILURE;
+  }
+
+  printf("socket created: %d\n", server_fd);
+  close(server_fd);
+
+  return EXIT_SUCCESS;
 }
